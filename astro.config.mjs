6cc0b50c191f4648cb2efc69a84ff722d8a8f47d
Blazing-mike <adebambomichael5@gmail.com>
2024-02-09 22:49:15 +0100

    astro islands

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://mike-astrotutorial.netlify.app"
+  site: "https://mike-astrotutorial.netlify.app",
+  integrations: [preact()]
 });
